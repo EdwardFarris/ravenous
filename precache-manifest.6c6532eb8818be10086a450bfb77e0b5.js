95306a01d56cbe9c7b997fd3d8cc7b4d6fa05e91
Edward Adam Farris <edwardfarris81@mail.com>
2019-11-01 13:19:45 -0700

    Updates

---
+++ precache-manifest.6c6532eb8818be10086a450bfb77e0b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f8c63c4256452cdcf71314c7ab2cd96",
+    "url": "/ravenous_app/index.html"
+  },
+  {
+    "revision": "35cde1d6e81956731410",
+    "url": "/ravenous_app/static/css/main.0ed8d251.chunk.css"
+  },
+  {
+    "revision": "867465d4912f38b16c3d",
+    "url": "/ravenous_app/static/js/2.e7f69930.chunk.js"
+  },
+  {
+    "revision": "35cde1d6e81956731410",
+    "url": "/ravenous_app/static/js/main.002eafbe.chunk.js"
+  },
+  {
+    "revision": "e9354d835cd4af552a95",
+    "url": "/ravenous_app/static/js/runtime-main.23bf39ec.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/ravenous_app/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/ravenous_app/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
