09a37a89eb4e3ccea740371d2828a9d3db3c5029
Edward Adam Farris <edwardfarris81@mail.com>
2019-11-07 11:36:39 -0700

    succefully making calls to Yelp API and dynamically rendering real businesses with various information

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,35 +1,48 @@
-import React from 'react';
-import './App.css';
-import BusinessList from '../BusinessList/BusinessList';
-import SearchBar from '../SearchBar/SearchBar';
+import React from "react";
+import "./App.css";
+import BusinessList from "../BusinessList/BusinessList";
+import SearchBar from "../SearchBar/SearchBar";
+import Yelp from "../../util/Yelp";
 // import logo from './logo.svg';
-const business = {
-  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Flavortown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90
-};
 
-const businesses = [business, business, business, business, business, business];
+// const business = {
+//   imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+//   name: 'MarginOtto Pizzeria',
+//   address: '1010 Paddington Way',
+//   city: 'Flavortown',
+//   state: 'NY',
+//   zipCode: '10101',
+//   category: 'Italian',
+//   rating: 4.5,
+//   reviewCount: 90
+// };
+
+// const businesses = [business, business, business, business, business, business];
 
 class App extends React.Component {
-  searchYelp(term, location, sortBy) {
-console.log("Searching Yelp with Pie, Phoenix, best_match")
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      businesses: []
+    };
+    this.searchYelp = this.searchYelp.bind(this);
   }
-  
+
+  searchYelp(term, location, sortBy) {
+    Yelp.searchYelp(term, location, sortBy).then((businesses) => {
+      this.setState({businesses: businesses });
+    });
+  }
+
   render() {
-  return (
-    <div className="App">
-    <h1>ravenous</h1>
-    <SearchBar searchYelp={this.searchYelp}/>
-    <BusinessList businesses={ businesses }/> 
-  </div>
-  );
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar searchYelp={this.searchYelp} />
+        <BusinessList businesses={this.state.businesses} />
+      </div>
+    );
   }
 }
 
